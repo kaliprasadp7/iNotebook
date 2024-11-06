3fcb7eb0075b738d5aee10a7bb80b6b40cfbb926
Kali Prasad Panda <kaliprasadp7@gmail.com>
2024-11-06 09:24:31 +0530

    Navbar and react router added with concurently added to run both frontend and backend server

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>This is iNotebook</h1>
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route exact path='/about' element={<About/>} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
