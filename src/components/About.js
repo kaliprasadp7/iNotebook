7da0f729291f239eaaff47fa7bc1fa72501d9c5f
Kali Prasad Panda <kaliprasadp7@gmail.com>
2024-11-06 21:46:22 +0530

    useLocation added to Navbar to blick active link

--- src/components/About.js
+++ src/components/About.js
@@ -1,20 +1,9 @@
-import React, { useContext, useEffect } from 'react'
-import NoteContext from '../context/notes/NoteContext'
+import React from 'react'
 
 const About = () => {
-  const a = useContext(NoteContext)
-  
-  useEffect(() => {
-    const newobj = {
-      "name" : "Amit",
-      "class" : "10B"
-  };
-    a.update(newobj); // Set the new value when the component mounts
-  }, []);
-
   return (
     <div>
-      <h2>This is about {a.state.name}</h2>
+      <h2>This is about</h2>
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,19 +1,9 @@
-import React, { useState } from 'react'
+import React from 'react'
 import NoteContext from './NoteContext'
 
 function NoteState(props) {
-    const s1 = {
-        "name" : "Kali Prasad",
-        "class" : "5B"
-    };
-    const [state, setState] = useState(s1);
-    const update = (value) => {
-        const s = setState(value);
-        return s;
-    }
-
   return (
-    <NoteContext.Provider value={{state, update}}>
+    <NoteContext.Provider value={{}}>
         {props.children}
     </NoteContext.Provider>
   )
