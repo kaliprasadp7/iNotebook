8fa71d56183cc777ca8784ad59f6743b91bb6ee3
Kali Prasad Panda <kaliprasadp7@gmail.com>
2024-11-06 20:36:57 +0530

    added useState for state value update in the context

--- src/components/About.js
+++ src/components/About.js
@@ -1,11 +1,20 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import NoteContext from '../context/notes/NoteContext'
 
 const About = () => {
   const a = useContext(NoteContext)
+  
+  useEffect(() => {
+    const newobj = {
+      "name" : "Amit",
+      "class" : "10B"
+  };
+    a.update(newobj); // Set the new value when the component mounts
+  }, []);
+
   return (
     <div>
-      <h2>This is about {a.name} and class is {a.class}</h2>
+      <h2>This is about {a.state.name}</h2>
     </div>
   )
 }
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NoteContext from './NoteContext'
 
 function NoteState(props) {
-    const state = {
+    const s1 = {
         "name" : "Kali Prasad",
-        "class" : "B25"
+        "class" : "5B"
+    };
+    const [state, setState] = useState(s1);
+    const update = (value) => {
+        const s = setState(value);
+        return s;
     }
+
   return (
-    <NoteContext.Provider value={state}>
+    <NoteContext.Provider value={{state, update}}>
         {props.children}
     </NoteContext.Provider>
   )
