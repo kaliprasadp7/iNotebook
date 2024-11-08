04983492570aacac92986d83ecaac4390aa96c3b
Kali Prasad Panda <kaliprasadp7@gmail.com>
2024-11-08 22:31:28 +0530

    Data fetched successfully from the backend

--- src/components/Notes.js
+++ src/components/Notes.js
@@ -1,11 +1,17 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import NoteContext from '../context/notes/NoteContext'
 import Noteitem from './Noteitem';
 import AddNote from './AddNote';
 
 const Notes = () => {
     const context = useContext(NoteContext);
-    const { notes } = context;
+    const { notes, getNotes } = context;
+    //Fetching data from the backend api once
+    useEffect(() => {
+        getNotes();
+        // eslint-disable-next-line
+    }, [])
+
     return (
         <>
             <AddNote />
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,64 +2,24 @@ import React, { useState } from 'react'
 import NoteContext from './NoteContext'
 
 function NoteState(props) {
-  const notesInitial = [
-    {
-      "_id": "6728f0cf802e42fd712b5528",
-      "user": "6727be1a3410877b93a687fa",
-      "title": "A story of a common man",
-      "description": "A man is very poor and operating a very small shop to live",
-      "tag": "personal",
-      "date": "2024-11-04T16:05:35.292Z",
-      "__v": 0
-    },
-    {
-      "_id": "6728f0d080fs2e42fd712b552a",
-      "user": "6727be1a3410877b93a687fa",
-      "title": "A story of a common man",
-      "description": "A man is very poor and operating a very small shop to live",
-      "tag": "personal",
-      "date": "2024-11-04T16:05:36.210Z",
-      "__v": 0
-    },
-    {
-      "_id": "6728f0cf802e4df2fd712b5528",
-      "user": "6727be1a3410877b93a687fa",
-      "title": "A story of a common man",
-      "description": "A man is very poor and operating a very small shop to live",
-      "tag": "personal",
-      "date": "2024-11-04T16:05:35.292Z",
-      "__v": 0
-    },
-    {
-      "_id": "6728f0d0802ef42fd712b552a",
-      "user": "6727be1a3410877b93a687fa",
-      "title": "A story of a common man",
-      "description": "A man is very poor and operating a very small shop to live",
-      "tag": "personal",
-      "date": "2024-11-04T16:05:36.210Z",
-      "__v": 0
-    },
-    {
-      "_id": "6728f0cf802ge42fd712b5528",
-      "user": "6727be1a3410877b93a687fa",
-      "title": "A story of a common man",
-      "description": "A man is very poor and operating a very small shop to live",
-      "tag": "personal",
-      "date": "2024-11-04T16:05:35.292Z",
-      "__v": 0
-    },
-    {
-      "_id": "6728f0d08eg02e42fd712b552a",
-      "user": "6727be1a3410877b93a687fa",
-      "title": "A story of a common man",
-      "description": "A man is very poor and operating a very small shop to live",
-      "tag": "personal",
-      "date": "2024-11-04T16:05:36.210Z",
-      "__v": 0
-    }
-  ];
+  const notesInitial = [];
+  const host = 'http://localhost:3001'
 
   const [notes, setNotes] = useState(notesInitial);
+
+  //Fetch all notes
+  const getNotes = async () => {
+    //API call
+    const response = await fetch(`${host}/api/notes/fetchallnotes`, {
+      method : 'GET',
+      headers : {
+        'Content-Type': 'application/json',
+        'auth-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjcyN2JlMWEzNDEwODc3YjkzYTY4N2ZhIn0sImlhdCI6MTczMDcwOTQ4MX0.ZPiWLuEixGQuhyr1F7JgpyuTy_MPzRJo7X2PVHlQBC0'
+      }
+    })
+    const json = await response.json();
+    setNotes(json);
+  }
 
   //Add a Note
   const addNote = (title, description, tag) => {
@@ -103,7 +63,7 @@ function NoteState(props) {
   }
 
   return (
-    <NoteContext.Provider value={{ notes, setNotes, addNote, deleteNote, editNote }}>
+    <NoteContext.Provider value={{ notes, setNotes, addNote, deleteNote, editNote, getNotes }}>
       {props.children}
     </NoteContext.Provider>
   )
